f696c11d544507b2a823765be88e32f87f646815
zengcd <2644903809@qq.com>
2020-03-14 16:35:39 +0800

    主页-楼层布局

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -37,6 +37,82 @@
     <image src='/static/uploads/icon_index_nav_4@2x.png'/>
   </navigator>
 </view>
+
+<!-- 樓層商品 -->
+<view class="floors">
+  <view class="floor">
+    <!-- 标题 -->
+    <view class="title">
+      <image src='/static/uploads/pic_floor01_title.png'/>
+    </view>
+    <!-- 条目 -->
+    <view class="items">
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_1@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_2@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_3@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_4@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_5@2x.png"></image>
+      </navigator>
+    </view>
+  </view>
+    <view class="floor">
+    <!-- 标题 -->
+    <view class="title">
+      <image src='/static/uploads/pic_floor01_title.png'/>
+    </view>
+    <!-- 条目 -->
+    <view class="items">
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_1@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_2@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_3@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_4@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_5@2x.png"></image>
+      </navigator>
+    </view>
+  </view>
+    <view class="floor">
+    <!-- 标题 -->
+    <view class="title">
+      <image src='/static/uploads/pic_floor01_title.png'/>
+    </view>
+    <!-- 条目 -->
+    <view class="items">
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_1@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_2@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_3@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_4@2x.png"></image>
+      </navigator>
+      <navigator url=''>
+        <image src="/static/uploads/pic_floor01_5@2x.png"></image>
+      </navigator>
+    </view>
+  </view>
+</view>
 	</view>
 </template>
 
@@ -83,6 +159,53 @@
   navigator {
     width: 128rpx;
     height: 140rpx;
+  }
+}
+.floor {
+  .title {
+    width: 750rpx;
+    height: 60rpx;
+    padding: 20rpx 0 0 10rpx;
+    background-color: #f4f4f4;
+  }
+  .items {
+    padding: 20rpx 16rpx;
+    overflow: hidden;
+    navigator {
+      float: left;
+      margin-left: 10rpx;
+      &:first-child {
+        width: 232rpx;
+        height: 386rpx;
+        margin-left: 0;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        width: 273rpx;
+        height: 188rpx;
+      }
+      &:nth-child(3),
+      &:nth-child(4) {
+        width: 193rpx;
+        height: 188rpx;
+      }
+      &:nth-last-child(-n + 2) {
+        margin-top: 10rpx;
+      }
+    }
+  }
+
+  &:first-child {
+    .items {
+      navigator {
+        width: 232rpx;
+        height: 188rpx;
+        &:first-child {
+          width: 232rpx;
+          height: 386rpx;
+        }
+      }
+    }
   }
 }
 </style>
