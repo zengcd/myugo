2c4f90595df8b4fb658acca81e11e267e42f2746
zengcd <2644903809@qq.com>
2020-03-14 21:46:26 +0800

    输入框获取焦点控制取消按钮的动作

--- src/components/search.vue
+++ src/components/search.vue
@@ -3,6 +3,7 @@
     <!-- 搜索栏标签 -->
     <view class="input-box">
       <input type="text" @focus='goSearch'/>
+      <text class='cancel' @click='handleCancel'>取消</text>
     </view>
     <!-- 搜索的结果 -->
     <view class="content"></view>
@@ -16,12 +17,21 @@ export default {
       isFocused: false
     }
   },
-  methods: {
+      methods: {
     goSearch () {
       console.log('hello')
       // 当输入框获取焦点时，在父元素添加一个类名 focused
       this.isFocused = true
-    }
+    },
+    goSearch () {
+      console.log('hello')
+      // 当输入框获取焦点时，在父元素添加一个类名 focused
+      this.isFocused = true
+    },
+    handleCancel () {
+      // 取消动作：恢复原始状态
+      this.isFocused = false
+  }
   }
 }
 </script>
@@ -34,11 +44,17 @@ export default {
 		    input {
 				background-color: #fff;
 				border-radius: 6rpx;
-			}
+            }
+            .cancel {
+      display: none;
+    }
         }
         &.focused {
     .input-box {
       background-color: #eee;
+    }
+    .cancel {
+      display: block;
     }
   }
 	}
