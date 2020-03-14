8d376cdee6b78c0ddbacf61757a9add4d7b5a5ff
zengcd <2644903809@qq.com>
2020-03-14 22:19:04 +0800

    搜索输入提示信息

--- src/components/search.vue
+++ src/components/search.vue
@@ -2,7 +2,7 @@
   <view class="search" :class="{focused: isFocused}">
     <!-- 搜索栏标签 -->
     <view class="input-box">
-      <input type="text" @focus="goSearch" />
+      <input :placeholder="placeholder" type="text" @focus="goSearch" />
       <text class="cancel" @click="handleCancel">取消</text>
     </view>
     <!-- 搜索的结果 -->
@@ -14,23 +14,21 @@
 export default {
   data() {
     return {
-      isFocused: false
+      isFocused: false,
+      placeholder:''
     };
   },
   methods: {
     goSearch() {
       console.log("hello");
       // 当输入框获取焦点时，在父元素添加一个类名 focused
-      this.isFocused = true;
-    },
-    goSearch() {
-      console.log("hello");
-      // 当输入框获取焦点时，在父元素添加一个类名 focused
-      this.isFocused = true;
+      this.isFocused = true
+      this.placeholder = "请输入想要的商品"
     },
     handleCancel() {
       // 取消动作：恢复原始状态
-      this.isFocused = false;
+      this.isFocused = false
+      this.placeholder = ""
     }
   }
 };
@@ -40,19 +38,51 @@ export default {
 .search {
   .input-box {
     background-color: #eb4450;
+    position: relative;
     padding: 20rpx 16rpx;
     display: flex;
     input {
       background-color: #fff;
+      padding-left: 55rpx;
       flex: 1;
     }
     .cancel {
       display: none;
     }
+    &::before {
+      content: "";
+      display: none;
+      width: 32rpx;
+      height: 32rpx;
+      position: absolute;
+      top: 50%;
+      transform: translate(15rpx, -50%);
+      background-image: url("http://static.botue.com/ugo/images/icon_search%402x.png");
+      background-size: 32rpx;
+    }
+    &::after {
+      content: "搜索";
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      padding-left: 40rpx;
+      transform: translate(-50%, -50%);
+      font-size: 24rpx;
+      color: #ccc;
+      background-image: url("http://static.botue.com/ugo/images/icon_search%402x.png");
+      background-size: 32rpx;
+      background-repeat: no-repeat;
+    }
   }
   &.focused {
     .input-box {
       background-color: #eee;
+      &::after {
+        display: none;
+      }
+      &::before {
+        display: block;
+      }
     }
     .cancel {
       display: block;
